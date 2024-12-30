ada098bddcaee46bb978ee28ef8cb59543828e87
PiyushJiwane <jiwanepiyush@gmail.com>
2024-12-30 08:37:44 +0530

    model

---
+++ models/transaction.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    userId: { type: mongoose.Types.ObjectId, required: true, ref:'admin' },
+    merchandTransactionId: { type: String, required: true },
+    transactionId: { type: String, required: true },
+    amount: { type: Number, required: true },
+},{
+    timestamps:true
+});
+
+const TransactionModel = mongoose.model('transaction', schema);
+
+module.exports=TransactionModel
