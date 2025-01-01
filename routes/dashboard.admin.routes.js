d3d8c1d6f55bc3df3696eb8c4dc6486807b5e165
PiyushJiwane <jiwanepiyush@gmail.com>
2025-01-01 19:25:33 +0530

    dashboard.admin.routes.js

--- routes/dashboard.admin.routes.js
+++ routes/dashboard.admin.routes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const isAuth = require("../utils/token.util");
-const { getUserList } = require("../controllers/dashboard.admin.controller");
+const { getUserList, updateSubscription } = require("../controllers/dashboard.admin.controller");
 
 require("dotenv").config()
 
 const dashboard_route = express.Router()
-const INITURL = process.env.INITURL
-console.log('INITURL',INITURL)
 
 dashboard_route.use(isAuth)
-dashboard_route.get(`${INITURL}/getUserList`, getUserList)
+dashboard_route.get(`/getUserList`, getUserList)
+dashboard_route.put(`/updateSubscription`, updateSubscription)
 
 module.exports = dashboard_route
