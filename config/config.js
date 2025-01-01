42d09382f8075f933152d94465b3ecf47f8568d7
PiyushJiwane <jiwanepiyush@gmail.com>
2025-01-01 19:22:46 +0530

    config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const cookieOptions = (days,token="refresh") => {
     return {
         httpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: "strict",
         maxAge: token==="access"?1000 * 15 : 1000 * 60 * 60 * 24 * days
     }
