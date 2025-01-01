90ff5296ede7e0e9bde4571efd187f713c45d8b5
PiyushJiwane <jiwanepiyush@gmail.com>
2025-01-01 19:23:41 +0530

    dashboard.admin.controller.js

--- controllers/dashboard.admin.controller.js
+++ controllers/dashboard.admin.controller.js
@@ -11,6 +11,20 @@ const getUserList = async (req, res) => {
     }
 }
 
+const updateSubscription = async (req, res) => {
+    try {
+        await TransactionModel.updateMany({
+            createdAt: {
+                $lt: new Date(new Date().setDate(new Date().getDate() - 30)), // Last 30 days
+            }
+        },{ subscription: false })
+        res.status(200).json({ success: true })
+    } catch (err) {
+        res.status(500).json({ success: false, error: err })
+    }
+}
+
 module.exports = {
-    getUserList
+    getUserList,
+    updateSubscription
 }
