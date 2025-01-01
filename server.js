85523b422c802f0f53b8569bd22af60a3589fe91
PiyushJiwane <jiwanepiyush@gmail.com>
2025-01-01 19:26:05 +0530

    server.js

--- server.js
+++ server.js
@@ -7,20 +7,28 @@ const cookieParser = require("cookie-parser");
 const payment_route = require("./routes/payments.phonepay.routes");
 const user_route = require("./routes/auth.user.routes");
 const dashboard_route = require("./routes/dashboard.admin.routes");
+const cors=require("cors")
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000
 const MONGO_URL = process.env.CONNECTION_STRING
+const INITURL = process.env.INITURL
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use(cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:4173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials:true
+}))
 
-app.use("/admin", admin_route)
-app.use("/user", user_route)
-app.use("/payment", payment_route)
-app.use("/dashboard/admin", dashboard_route)
+app.use(`${INITURL}/admin`, admin_route)
+app.use(`${INITURL}/user`, user_route)
+app.use(`${INITURL}/payment`, payment_route)
+app.use(`${INITURL}/dashboard/admin`, dashboard_route)
 
 const startServer = async () => {
     try {
